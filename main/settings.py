c190a82993f662c64f77c940f9172134fdeaf486
Marko <tmarkec@gmail.com>
2023-11-01 13:42:25 +0000

    add navbar and footer

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,8 @@ if os.path.exists('env.py'):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELEPMENT' in os.environ
+DEBUG = True
+# 'DEVELEPMENT' in os.environ
 
 ALLOWED_HOSTS = ['*']
 
@@ -121,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
